f4983ae5842aa418c38dad8b31178a9f93dfcf8b
Richard <richardshuai98@gmail.com>
2020-07-21 22:07:26 -0700

    Refactoring to migrate code to .py

---
+++ src/layers.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import layers
+
+# Batchnorm epsilon
+BN_EPS = 1e-4
+
+# Use float16
+# tf.keras.backend.set_floatx('float16')
+
+class ConvBnRelu2d(layers.Layer):
+    # Convolutional -> Batch norm -> ReLU
+    
+    def __init__(self, out_channels, kernel_size=(3, 3), padding='same', dilation_rate=1):
+        super(ConvBnRelu2d, self).__init__()
+        self.conv = layers.Conv2D(filters=out_channels, kernel_size=kernel_size, padding='same', 
+                                  dilation_rate=dilation_rate, use_bias=False)
+        self.bn = layers.BatchNormalization(epsilon=BN_EPS)
+        self.relu = layers.ReLU()
+        
+    def call(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+class StackEncoder(layers.Layer):
+    # ConvBnRelu -> ConvBnRelu -> * -> max pool (2x2)
+    #      * store tensor for concatenation with expansive path
+
+    def __init__(self, y_channels, kernel_size=3, dilation_rate=1):
+        super(StackEncoder, self).__init__()
+        self.encode = keras.Sequential([
+            ConvBnRelu2d(y_channels, kernel_size=kernel_size, padding='same', dilation_rate=dilation_rate),
+            ConvBnRelu2d(y_channels, kernel_size=kernel_size, padding='same', dilation_rate=dilation_rate)
+        ])
+        
+        self.max_pool = layers.MaxPool2D(pool_size=2, strides=2)
+        
+    def call(self, x):
+        x = self.encode(x)
+        x_small = self.max_pool(x)
+        return x, x_small
+
+
+class StackDecoder(layers.Layer):
+    # Upsample (2x) -> concatenation -> ConvBnRelu -> ConvBnRelu -> ConvBnRelu
+
+    def __init__(self, y_channels, kernel_size=3, dilation_rate=1):
+        super(StackDecoder, self).__init__()
+        self.decode = keras.Sequential([
+            ConvBnRelu2d(y_channels, kernel_size=kernel_size, padding='same', dilation_rate=dilation_rate),
+            ConvBnRelu2d(y_channels, kernel_size=kernel_size, padding='same', dilation_rate=dilation_rate),
+            ConvBnRelu2d(y_channels, kernel_size=kernel_size, padding='same', dilation_rate=dilation_rate),
+            ]
+        )
+        
+        
+    def call(self, x, down_tensor):
+        x = layers.UpSampling2D(size=(2, 2), interpolation='bilinear')(x)
+        
+        # Calculate cropping for down_tensor to concatenate with x
+        _, h2, w2, _ = down_tensor.shape
+        _, h1, w1, _ = x.shape
+        h_diff, w_diff = h2 - h1, w2 - w1
+        
+        cropping = ((int(np.ceil(h_diff / 2)), int(np.floor(h_diff / 2))),
+                    (int(np.ceil(w_diff / 2)), int(np.floor(w_diff / 2))))
+        down_tensor = layers.Cropping2D(cropping=cropping)(down_tensor)        
+        x = layers.concatenate([x, down_tensor], axis=3)
+        x = self.decode(x)
+        return x
---
+++ src/model.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import layers
+
+class UNet_2d(keras.Model):
+    def __init__(self):
+        super(UNet_2d, self).__init__()
+        self.down1 = StackEncoder(24, kernel_size=3, dilation_rate=4)
+        self.down2 = StackEncoder(64, kernel_size=3, dilation_rate=4)
+        self.down3 = StackEncoder(128, kernel_size=3, dilation_rate=4)
+        self.down4 = StackEncoder(256, kernel_size=3, dilation_rate=4)
+        self.down5 = StackEncoder(512, kernel_size=3, dilation_rate=4)
+        self.down6 = StackEncoder(1024, kernel_size=3, dilation_rate=4)
+
+        self.up6 = StackDecoder(512, kernel_size=3, dilation_rate=4)
+        self.up5 = StackDecoder(256, kernel_size=3, dilation_rate=4)
+        self.up4 = StackDecoder(128, kernel_size=3, dilation_rate=4)
+        self.up3 = StackDecoder(64, kernel_size=3, dilation_rate=4)
+        self.up2 = StackDecoder(24, kernel_size=3, dilation_rate=4)
+        self.up1 = StackDecoder(24, kernel_size=3, dilation_rate=4)
+        
+        # Final prediction uses a single feature channel (green)
+        self.classify = layers.Conv2D(filters=1, kernel_size=1, use_bias=True)
+        
+        self.center = ConvBnRelu2d(1024, kernel_size=3, padding='same')
+        
+        
+    def call(self, x):
+        out = x;
+        down1_tensor, out = self.down1(out)
+        down2_tensor, out = self.down2(out)
+        down3_tensor, out = self.down3(out)
+        down4_tensor, out = self.down4(out)
+        down5_tensor, out = self.down5(out)
+        down6_tensor, out = self.down6(out)
+
+        out = self.center(out)
+
+        out = self.up6(out, down6_tensor)
+        out = self.up5(out, down5_tensor)
+        out = self.up4(out, down4_tensor)
+        out = self.up3(out, down3_tensor)
+        out = self.up2(out, down2_tensor)
+        out = self.up1(out, down1_tensor)
+
+        out = self.classify(out)
+        out = tf.squeeze(out, axis=3)
+        
+        return out
